efcef21620b4ad4ba5810501f5e35347d82fa96d
MINFIN\amarildo.ferreira <amarildo.ferreira@minfin.gov.ao>
2020-11-29 15:04:48 +0000

    #10-Displaying Data: Two way Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AuthorsComponent } from './authors/authors.component';
     AuthorsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     CoursesService,
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,19 +4,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <input type='text' (keyup.enter)="onkeyup()">
+    <input type='text' [(ngModel)]="email" (keyup.enter)="onkeyup()">
     <button class='btn btn-primary' (click)="onClick($event)">save</button>
   `
 })
 export class CoursesComponent {
 
   isActive = true;
+  email = 'me@domain.com';
 
   onkeyup(){
-    console.log('ENTER was clicked...');
+    console.log('Valor: ', this.email);
   }
 
-  onClick(event){
-    console.log('Event: ', event);
+  onClick(){
+    console.log('Email: ', this.email);
   }
 }
