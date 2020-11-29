a49ece38fc55be9f7add5bc4a5da4cb2abc956bc
MINFIN\amarildo.ferreira <amarildo.ferreira@minfin.gov.ao>
2020-11-29 14:44:01 +0000

    #8-Displaying Data: Event filtering

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,10 +4,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <button class='btn btn-primary' [style.color]="isActive ? 'red' : 'white'">save</button>
+    <input type='text' (keyup.enter)="onkeyup()">
+    <button class='btn btn-primary' (click)="onClick($event)">save</button>
   `
 })
 export class CoursesComponent {
 
   isActive = true;
+
+  onkeyup(){
+    console.log('ENTER was clicked...');
+  }
+
+  onClick(event){
+    console.log('Event: ', event);
+  }
 }
