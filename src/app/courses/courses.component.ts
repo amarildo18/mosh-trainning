bcdf9ec42271dac9c5ac76a054f8ca47a38627d5
MINFIN\amarildo.ferreira <amarildo.ferreira@minfin.gov.ao>
2020-11-29 13:30:33 +0000

    #4-Displaying Data: Adicionando Bootstrap

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,18 +4,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <h2>{{teste}}</h2>
-    <ul>
-      <li *ngFor="let course of courses">{{course}}</li>
-    </ul>
+    <button class='btn btn-primary'>save</button>
   `
 })
 export class CoursesComponent {
 
-  teste = 'Lista de Cursos';
-  courses: string[];
-
-  constructor(coursesService: CoursesService){
-    this.courses = coursesService.getCourses();
-  }
 }
