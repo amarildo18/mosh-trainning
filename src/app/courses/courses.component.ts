0301549ab6107bad9cc39f6d8a2cbd89b31dc689
MINFIN\amarildo.ferreira <amarildo.ferreira@minfin.gov.ao>
2020-11-29 12:06:03 +0000

    #3.6 - Directivas

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  template: '<h2>Cursos</h2>'
+  template: `
+    <h2>{{teste}}</h2>
+    <ul>
+      <li *ngFor="let course of courses">{{course}}</li>
+    </ul>
+  `
 })
 export class CoursesComponent {
 
+  teste = 'Lista de Cursos';
+  courses = ['Curso1', 'Curso2', 'Curso3'];
 }
