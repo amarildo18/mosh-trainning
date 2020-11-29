e0a82c8c626e929f56902f4d1224b47eb97d4360
MINFIN\amarildo.ferreira <amarildo.ferreira@minfin.gov.ao>
2020-11-29 15:27:47 +0000

    #12-Displaying Data: Pipes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,20 +4,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <input type='text' [(ngModel)]="email" (keyup.enter)="onkeyup()">
-    <button class='btn btn-primary' (click)="onClick($event)">save</button>
+    <p>{{course.title | uppercase}}</p>
+    <p>{{course.rating | number: '1.2-2'}}</p>
+    <p>{{course.students | number}}</p>
+    <p>{{course.price | currency}}</p>
+    <p>{{course.releaseDate | date}}</p>
   `
 })
 export class CoursesComponent {
 
-  isActive = true;
-  email = 'me@domain.com';
+  course = {
+    title: 'The complete Angular course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  };
 
-  onkeyup(){
-    console.log('Valor: ', this.email);
-  }
-
-  onClick(){
-    console.log('Email: ', this.email);
-  }
 }
