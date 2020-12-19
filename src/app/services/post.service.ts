1f2c2c76c9dd5cc7bde22fba61916654c63b1583
MINFIN\amarildo.ferreira <amarildo.ferreira@minfin.gov.ao>
2020-12-19 13:21:06 +0000

    Extracting a reusable handleError

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -18,34 +18,41 @@ export class PostService {
 
   getPost(){
     return this.http.get(this.url)
+      .pipe(
+        catchError(this.handleError)
+      )
   }
 
   createPost(post){
     return this.http.post(this.url, JSON.stringify(post))
       .pipe(
-        catchError((error: Response) => {
-          if(error.status === 400 )
-            return Observable.throw( new BadRequestError(error.json()))
-
-          return Observable.throw(new AppError(error.json()))
-        })
+        catchError(this.handleError)
       );
   }
 
   updatePost(post){
 
     return this.http.patch(this.url + '/'+post.id, JSON.stringify({isRead: true}))
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   deletePost(id){
     return this.http.delete(this.url+'/'+id)
       .pipe(
-        catchError((error: Response) => {
-          if(error.status === 404)
-            return Observable.throw( new NotFoundError());
-
-          return Observable.throw( new AppError(error));
-        })
+        catchError(this.handleError)
       );
+  }
+
+  private handleError(error: Response){
+
+    if(error.status === 400 )
+      return Observable.throw( new BadRequestError(error.json()));
+
+    if(error.status === 404)
+      return Observable.throw( new NotFoundError());
+
+    return Observable.throw(new AppError(error.json()))
   }
 }
